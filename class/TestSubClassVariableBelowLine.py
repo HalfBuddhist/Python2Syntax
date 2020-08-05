3a7b3bbbc11c0dd92254b557c634fdc7b52a3478
liuqw <liuqingwei2019@gmail.com>
2020-08-05 19:11:40 +0800

    chore: add more specific comment.

--- class/TestSubClassVariableBelowLine.py
+++ class/TestSubClassVariableBelowLine.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 # coding=utf-8
 '''
-测试下划线变量的继承性
+测试下划线变量的继承性:
+Conclusion:
+    单下划线的为保护变量，可以继承；
+    双下划线的为私有变量，不可以直接继承，只能通过_P_VarName的形式来访问；
+    以下规则既适用于变量，也适用于成员函数；
 '''
 
 class P(object):
     __a__ = 1
 
-    def __hello__(self):
+    def __hello(self):
         print "hello called"
 
     def __init__(self, pa = 100):
         self.__b__ = pa
+        self.__ts_pri = "hello"
 
 class C(P):
     __a = 2
@@ -24,8 +29,13 @@ class C(P):
 
 if __name__ == '__main__':
     # print C.__a__
-    # C().__hello__()
+    # C()._C__hello()
     # C()._P__hello()
     # C().test()
-    print C.__a
+    # print C.__a
     # print C()._P__b__
+
+
+    # 测试对于私有变量的访问
+    p = P()
+    print p._P__ts_pri
